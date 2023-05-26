c90766eb3fd4fb8831db751bd4493ee4b69f3d0b
Memo <60975465+guillermemo1833@users.noreply.github.com>
2023-05-25 22:08:43 -0400

    Update consumer.py

--- Kafka/Consumer/consumer.py
+++ Kafka/Consumer/consumer.py
@@ -1,1 +1,24 @@
 
+import json
+from kafka import KafkaConsumer
+
+def sucursal(id_sucursal, data):
+    print(f"Sucursal {id_sucursal} - Datos: {data}")
+
+def main():
+    sucursales = 3
+    servidores_bootstrap = 'kafka:9092'
+    topic = 'camiones_grandes'
+    print("Esperando mensajes...")
+
+    consumer = KafkaConsumer(topic, bootstrap_servers=[servidores_bootstrap])
+
+    for i in range(sucursales):
+        for message in consumer:
+            data = json.loads(message.value.decode('utf-8'))
+            sucursal(i + 1, data)
+
+
+
+if __name__ == "__main__":
+    main()
